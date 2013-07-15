1853e1045f49a63bf8f1ad9830c3d2da5404be56
Masaya Ozawa <ma3ya.ozw+github@gmail.com>
2013-07-15 13:46:47 +0000

    値渡し関数の実装
    値渡しの関数をインスタンスメソッドとして呼べるように実装。
    参照渡しの関数を読んだ場合、例外をなげ対応していないことを示す。

--- src/M3y/Tooo/Tooo.php
+++ src/M3y/Tooo/Tooo.php
@@ -1,6 +1,38 @@
 <?php
 namespace M3y\Tooo;
 
+/**
+ * To object-oriented.
+ */
 class Tooo
 {
+    /**
+     * 関数の実行。
+     *
+     * phpに定義されている関数をオブジェクトのメソッドとして利用できるようにする。<br />
+     * ただし、値渡しで実行される関数のみ。
+     *
+     * @param  string $name      関数名
+     * @param  array  $arguments 関数の引数
+     * @return mixed  実行結果
+     */
+    public function __call($name, $arguments)
+    {
+        if (!function_exists($name)) {
+            throw new \BadFunctionCallException("undefined function. [$name]");
+        }
+
+        $reflectionFunction = new \ReflectionFunction($name);
+        $reflectionParameters = $reflectionFunction->getParameters();
+
+        foreach ($reflectionParameters as $parameter) {
+            if ($parameter->isPassedByReference()) {
+                throw new \InvalidArgumentException(
+                    'sorry, it does not support the use of pass-by-reference.'
+                );
+            }
+        }
+
+        return $reflectionFunction->invokeArgs($arguments);
+    }
 }
--- tests/M3y/Tooo/ToooTest.php
+++ tests/M3y/Tooo/ToooTest.php
@@ -1,13 +1,72 @@
 <?php
 namespace M3y\Tooo\ToooTest;
 
+use M3y\Tooo\Tooo;
+
 class ToooTest extends \PHPUnit_Framework_TestCase
 {
+    private $oo;
+
+    /**
+     * setup.
+     */
+    public function setUp()
+    {
+        $this->oo = new Tooo;
+    }
+
     /**
      * @test
      */
-    public function クラスが存在すること()
+    public function 値渡しの関数を実行できること_strtoupper()
     {
-        $this->assertTrue(class_exists('M3y\Tooo\Tooo'));
+        // strtoupper
+        $expected = strtoupper("Test_Value");
+        $this->assertSame($expected, $this->oo->strtoupper('Test_Value'));
+    }
+
+    /**
+     * @test
+     */
+    public function 値渡しの関数を実行できること_strtolower()
+    {
+        // strtolower
+        $expected = strtolower("tEST_vALUE");
+        $this->assertSame($expected, $this->oo->strtolower('tEST_vALUE'));
+    }
+
+    /**
+     * @test
+     */
+    public function 値渡しの関数を実行できること_array_keys()
+    {
+        // array_keys
+        $sample = array(
+            "test_key2" => "hoge",
+            "hogehoge",
+            "test_key1" => null,
+        );
+
+        $expected = array_keys($sample);
+        $this->assertSame($expected, $this->oo->array_keys($sample));
+    }
+
+    /**
+     * @test
+     * @expectedException BadFunctionCallException
+     */
+    public function 定義されていない関数を呼ばれた際に例外が投げられること()
+    {
+        $this->oo->undefined();
+    }
+
+    /**
+     * @test
+     * @expectedException InvalidArgumentException
+     */
+    public function 未対応の参照渡しの関数の場合、例外が投げられること()
+    {
+        $matches = array();
+        $this->oo->preg_match('/[a-z]/', 'test01234', $matches);
     }
 }
